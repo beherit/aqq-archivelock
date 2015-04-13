5217f3c29587987663596f7924f2325329e524c4
Krzysztof Grochocki <sirbeherit@gmail.com>
2015-04-13 17:48:52 +0200

    * Usuni?cie drobnego b??du przy zapisie has?a (niepotrzebna zmiana na ma?e litery).

--- ArchiveLock.cpp
+++ ArchiveLock.cpp
@@ -602,7 +602,7 @@ extern "C" PPluginInfo __declspec(dllexport) __stdcall AQQPluginInfo(DWORD AQQVe
 {
 	PluginInfo.cbSize = sizeof(TPluginInfo);
 	PluginInfo.ShortName = L"ArchiveLock";
-	PluginInfo.Version = PLUGIN_MAKE_VERSION(1,1,1,0);
+	PluginInfo.Version = PLUGIN_MAKE_VERSION(1,1,2,0);
 	PluginInfo.Description = L"Blokuje dostêp do archiwum rozmów na has³o. Domyœlne has³o to \"lock\".";
 	PluginInfo.Author = L"Krzysztof Grochocki";
 	PluginInfo.AuthorMail = L"kontakt@beherit.pl";
